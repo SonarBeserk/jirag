ad36165e68afc6c3261d36c0c26b4178ef0c675c
SonarBeserk <sonarbeserk@gmail.com>
2019-11-07 00:08:47 -0500

    Update jira client creation to warn when not signed in

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"errors"
 	"strings"
 
 	"github.com/andygrunwald/go-jira"
@@ -8,6 +9,10 @@ import (
 
 // NewJiraClient creates a new instance of a jira client
 func NewJiraClient(url string, username string, password string) (*jira.Client, error) {
+	if url == "" || username == "" || password == "" {
+		return nil, errors.New("Invalid credentials detected, please login to call this command")
+	}
+
 	transport := jira.BasicAuthTransport{
 		Username: strings.TrimSpace(username),
 		Password: strings.TrimSpace(password),
